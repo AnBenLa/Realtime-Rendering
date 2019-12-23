46300c4c2d4284202373c19a9ab43bb1eacaf24f
AnBenLa <antonbenlammert@gmail.com>
2019-12-23 15:38:20 +0100

    Worked on exercise 3.1. The processing of an intersection event needs to be handled. Besides this the program seems to be working as intended.

--- assignment3/source/exercise31.cpp
+++ assignment3/source/exercise31.cpp
@@ -8,13 +8,14 @@
 #include <queue>
 #include <map>
 #include <algorithm>
+#include <iomanip>
 
 #define us unsigned  short
 
 using namespace std;
 
 struct point{
-    short x,y;
+    double x,y;
     string id;
 };
 
@@ -37,13 +38,71 @@ bool operator > (segment const& s1, segment const& s2){
     return (s1.a.y > s2.a.y && s1.a.y > s2.b.y) || (s1.b.y > s2.a.y && s1.b.y > s2.b.y);
 }
 
+ostream& operator << (ostream& os, vector<event>const& events){
+    cout << setprecision(1) << fixed;
+    for(auto const& elem : events){
+        os << "("<< elem.p.x << " , " << elem.p.y << ") ";
+    }
+    return os;
+}
+
+ostream& operator << (ostream& os, vector<segment>const& segments){
+    for(auto const& seg : segments){
+        os << seg.id << " ";
+    }
+    return os;
+}
+
+//siehe ORI2 function from slide 48
+//returns -1 for ccw, +1 for cw and 0 if b is collinear with the line through a and c
+int orientation(point const& a, point const& b, point const& c){
+    double dx21 = b.x - a.x;
+    double dy21 = b.y - a.y;
+    double dx31 = c.x - a.x;
+    double dy31 = c.y - a.y;
+    if(dy31*dx21 > dx31*dy21)
+        return - 1;
+    if(dy31*dx21 < dx31*dy21)
+        return +1;
+    if(dx31*dx21 < 0 || dy21*dy31 < 0)
+        return -1;
+    if(dx31*dx31 + dy31*dy31 > dx21*dx21 + dy21*dy21)
+        return +1;
+    return 0;
+}
+
+//http://www.softwareandfinance.com/Visual_CPP/VCPP_Intersection_Two_lines_EndPoints.html
+void intersection_check(segment const& a, segment const& b, vector<event>& events){
+    bool intersection = orientation(a.a, a.b, b.a)*orientation(a.a,a.b,b.b) <= 0
+                        && orientation(b.a,b.b,a.a)*orientation(b.a,b.b,a.b) <= 0;
+    if(intersection){
+        double dx1,dx2,dy1,dy2,m1,m2,c1,c2;
+        dx1 = a.b.x - a.a.x;
+        dy1 = a.b.y - a.a.y;
+        m1 = dy1 / dx1;
+        c1 = a.a.y - m1 * a.a.x;
+        dx2 = b.b.x - b.a.x;
+        dy2 = b.b.y - b.a.y;
+        m2 = dy2 / dx2;
+        c2 = b.a.y - m2 * b.a.x;
+        point intersect{(c2-c1)/(m1-m2), m1*(c2-c1)/(m1-m2)+c1};
+        events.push_back(event{true,a.id,b.id,intersect});
+        sort(events.begin(), events.end(),[](event const& a, event const& b) -> bool{return a.p.y > b.p.y;});
+    }
+}
+
 void linesweep(map<string,segment>& segments, vector<point>& points){
     sort(points.begin(), points.end(),[](point const& a, point const& b) -> bool{return a.y > b.y;});
     vector<segment> active_segments;
-    queue<event> event_queue;
+    vector<event> event_queue;
+    event_queue.reserve(points.size());
+
+    //add all the points to the event queue
     for(auto const& p : points){
-        event_queue.push(event{false,NULL,NULL, p});
+        event_queue.push_back(event{false,"","", p});
     }
+#
+    //process all elements in the queue
     while(!event_queue.empty()){
         event current_event = event_queue.front();
         if(current_event.intersection){
@@ -76,26 +135,48 @@ void linesweep(map<string,segment>& segments, vector<point>& points){
                 active_segments.erase(segment_it);
             } else {
                 /*
-                    TODO implement this in the correct way
                     add the segment to the active segment list in the right position and check for intersections with neighbours
                     the right position is defined by the orientation of the point that should be processed in relation
                     to the active segments. If the point p is left of an segment a the segment of the point p should be left of the segment a
                     if an intersection exists create an intersection event with the intersecting segment ids and push ist to the queue
                 */
-                active_segments.push_back(current_segment);
+                if(!active_segments.empty()) {
+                    bool segment_added = false;
+                    int index = 0;
+                    for (auto it = active_segments.begin(); it != active_segments.end(); it++) {
+                        if (orientation(it->a, current_event.p, it->b) == 1) {
+                            index += 1;
+                            continue;
+                        } else {
+                            active_segments.insert(it, current_segment);
+                            segment_added = true;
+                            break;
+                        }
+                    }
+                    if(!segment_added)
+                        active_segments.push_back(current_segment);
+                    if(index - 1 >= 0)
+                        intersection_check(current_segment, active_segments[index - 1], event_queue);
+                    if(index + 1 < active_segments.size())
+                        intersection_check(current_segment, active_segments[index + 1], event_queue);
+                } else {
+                    active_segments.push_back(current_segment);
+                }
             }
         }
-        event_queue.pop();
+        event_queue.erase(event_queue.begin());
+        cout << active_segments << "\n";
+        cout << event_queue << "\n";
     }
 }
 
 int main (){
     us n;
-    short x0,y0,x1,y1;
+    double x0,y0,x1,y1;
     string id;
     map<string,segment> segments;
     vector<point> points;
-    ifstream cin ("C:\\Users\\Anton\\CLionProjects\\Realtime-Rendering\\assignment3\\source\\input.txt");
+    ifstream cin ("C:\\Users\\Elke\\Desktop\\Anton\\Realtime-Rendering\\assignment3\\source\\input.txt");
     cin >> n;
     while(n--){
         cin >> id >> x0 >> y0 >> x1 >> y1;
